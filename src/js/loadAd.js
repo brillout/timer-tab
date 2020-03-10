caec6ce924a7bfb0e0a56616c79664eb87bd3b5f
Romuald Brillout <github.com@brillout.com>
2020-03-10 12:34:38 +0100

    fix ad loading bug

--- src/js/loadAd.js
+++ src/js/loadAd.js
@@ -4,7 +4,7 @@ export default () => {
   const AD_SLOTS = [
     {
       slotID: 'div-gpt-ad-1583509610935-0',
-      slotName: '/21735472908/TIMER-TAB_leaderboard_ATF_desktop ',
+      slotName: '/21735472908/TIMER-TAB_leaderboard_ATF_desktop',
       slotSize: [728, 90],
     },
     {
