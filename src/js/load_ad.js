77b7e07b5746a8473add56913d6381bab1e628fb
Romuald Brillout <github.com@brillout.com>
2020-03-16 12:28:38 +0100

    add secondary ad

--- src/js/load_ad.js
+++ src/js/load_ad.js
@@ -7,6 +7,16 @@ export default () => {
       slotName: '/21735472908/TIMER-TAB_leaderboard_ATF_desktop',
       slotSize: [728, 90],
     },
+    {
+      slotID: 'div-gpt-ad-1584356029392-0',
+      slotName: '/21735472908/TIMER-TAB_leaderboard_BTF_desktop',
+      slotSize: [728, 90],
+    },
+    {
+      slotID: 'div-gpt-ad-1584356153147-0',
+      slotName: '/21735472908/TIMER-TAB_leaderboard_BTF_mobile',
+      slotSize: [320, 50],
+    },
   ];
 
   load_ad(AD_SLOTS, {useOneTag: true});
