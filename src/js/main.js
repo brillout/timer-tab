b8ff43902e04dbf826a4bc76f40842569c09be4e
Romuald Brillout <github.com@brillout.com>
2020-03-20 00:19:30 +0100

    adapt to new pretty_scroll_area

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 //console todo; get seo stats before uploading this version containing mod desc
 import 'regenerator-runtime/runtime';
 import ml from './ml';
-import 'tab-utils/more_panel';
+import more_panel from 'tab-utils/more_panel';
 import load_ad from './load_ad';
 import load_timer from './load_timer';
 import auto_remove_hash from 'tab-utils/auto_remove_hash';
@@ -10,6 +10,8 @@ import youtube_alarm from './youtube_alarm';
 
 window.onload = () => {
   deprecate_old_browsers({projectName: 'Timer Tab', email: 'tim'+'ertab@br'+'illout.com'});
+
+  more_panel();
 
   load_timer();
 
