e06e5471d4a9b4a220384fff394e84246aed16ca
Romuald Brillout <github.com@brillout.com>
2020-04-09 13:23:57 +0200

    rebase on tab-utils - wip

--- views/TimerPage/PRESETS.ts
+++ views/TimerPage/PRESETS.ts
@@ -2,15 +2,18 @@ export {PRESETS};
 
 const PRESETS = {
    classic: {
-    'timer_background_image': '#FFFFFF',
+    'timer_background_color': '#FFFFFF',
     'timer_youtube_alarm': 'https://youtu.be/PS5KAEgrtMA',
    },
    dark: {
-    'timer_background_image': '#222222',
+    // Alternatives
+    // - https://imgur.com/a/9s0S6Ba
+    'timer_background_image': 'https://i.imgur.com/lornMTz.jpg',
     'timer_youtube_alarm': 'https://www.youtube.com/watch?v=ziX9i7VKLQU',
    },
    'miami_night': {
     'timer_background_image': 'https://i.pinimg.com/originals/55/da/1c/55da1c061ea41c054cb09c35554ed81b.jpg',
-    'timer_youtube_alarm': 'https://www.youtube.com/watch?v=zZ0zPbh1QHE&feature=youtu.be&t=78',
+    // Original: https://www.youtube.com/watch?v=zZ0zPbh1QHE&feature=youtu.be&t=78
+    'timer_youtube_alarm': 'https://www.youtube.com/watch?v=V6bhe6kiUko&t=78',
    }
 };
