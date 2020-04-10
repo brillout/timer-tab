4282e5fe5a1b90254dbba534542ec663971d4c3c
Romuald Brillout <github.com@brillout.com>
2020-04-10 22:26:06 +0200

    minor

--- views/TimerPage/PRESETS.ts
+++ views/TimerPage/PRESETS.ts
@@ -12,8 +12,12 @@ const PRESETS = {
     'timer_youtube_alarm': 'https://www.youtube.com/watch?v=ziX9i7VKLQU',
    },
    'miami_night': {
-    'timer_background_image': 'https://i.pinimg.com/originals/55/da/1c/55da1c061ea41c054cb09c35554ed81b.jpg',
+    'timer_background_image': 'https://i.imgur.com/3lWI71b.jpg',
     // Original: https://www.youtube.com/watch?v=zZ0zPbh1QHE&feature=youtu.be&t=78
     'timer_youtube_alarm': 'https://www.youtube.com/watch?v=V6bhe6kiUko&t=78',
-   }
+   },
+   'the_machine': {
+    'timer_background_image': 'https://i.imgur.com/hBLBrCy.jpg',
+    'timer_youtube_alarm': 'https://youtu.be/nUtAWeSK28g?t=97',
+   },
 };
