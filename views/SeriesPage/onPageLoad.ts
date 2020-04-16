0904eb663e92b6be718ac3055ff5e126879e06b6
Romuald Brillout <github.com@brillout.com>
2020-04-16 18:49:25 +0200

    implement Timer Series - wip

--- views/SeriesPage/onPageLoad.ts
+++ views/SeriesPage/onPageLoad.ts
@@ -1,5 +1,8 @@
+import {render_view} from './render_view';
+
 export {onPageLoad};
 
-function onPageLoad() {
-    ;
+async function onPageLoad(load_common, goldpage_args) {
+    render_view(goldpage_args);
+    load_common();
 }
---
+++ views/SeriesPage/render_view.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export {render_view};
+
+function render_view(goldpage_args) {
+    const { page, initialProps, CONTAINER_ID } = goldpage_args;
+    const element = React.createElement(page.view, initialProps);
+    const container = document.getElementById(CONTAINER_ID);
+    if (page.renderToHtml) {
+      ReactDOM.hydrate(element, container);
+    } else {
+      ReactDOM.render(element, container);
+    }
+}
